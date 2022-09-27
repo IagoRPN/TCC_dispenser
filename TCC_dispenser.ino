6b1982616ad4fdca7eec4962d718c45e4954700a
Iago <iagotpn@gmail.com>
2022-09-27 07:38:17 -0300

    definição dos pinos da placa

--- TCC_dispenser.ino
+++ TCC_dispenser.ino
@@ -4,30 +4,51 @@
 #include <WiFi.h> //biblioteca do wifi
 #include "time.h" //biblioteca de informações de horas
 
-#define tamanho_rotina 60 //quantidade de horarios disponiveis para programar
 
-#define ssid  "Ranadaia"; //ssid do wifi
-#define password "D@vi2410"; //senha do wifi
+#define ssid  "Ranadaia" //ssid do wifi
+#define password "D@vi2410" //senha do wifi
 #define firebase_host "https://dispensador-remedios-default-rtdb.firebaseio.com/" //host do firebase 
-#define firebase_auth "token_or_secret" //chave de autenticação do firebase
+#define firebase_auth "pLRZuYhSqpWbvoV6fPs9rgpc3PqRi79qAiOhcCC3" //chave de autenticação do firebase
 
-//estrutura de dados para armazenar dados da rotina
-struct DadosRotina {  
-  int hora;
-  int minuto;
-  int slot;
-  char* nome;
-  };
+//Definição dos pinos
+#define PINO_MOTOR1 13
+#define PINO_MOTOR2 12
+#define PINO_MOTOR3 14
+#define PINO_MOTOR4 27
+#define PINO_EN1 26
+#define PINO_EN2 25
+#define PINO_EN3 33
+#define PINO_EN4 32
+#define PINO_EN5 4
+#define PINO_EN6 2
+#define PINO_SENSOR 36
+#define PINO_LCD_TX 23
+#define PINO_LCD_RX 22
+#define PINO_BOTAO 39
+#define PINO_BUZZER 15
 
 const char* ntpServer = "0.br.pool.ntp.org"; //endereço do servidor de tempo
 const long  gmtOffset_sec = -3 * 3600; //config do fuso horario br
 const int   daylightOffset_sec = 0; //horário de verão
 
-/*
-DadosRotina horario_rotina[tamanho_rotina] = {
-  
-  };
+const int tam_list = 2;
 
+//estrutura de dados para armazenar dados da rotina
+typedef struct {
+  int hora;
+  int minuto;
+  int slot;
+  String nome;
+} DadosRemedio;
+
+DadosRemedio rotina[tam_list];
+
+void AtualizaLista() {
+  for (int i = 0; i <= tam_list - 1; i++){
+   rotina[i].hora = Firebase.getInt("/"+String(i)+"/hora");
+   rotina[i].minuto = Firebase.getInt("/"+String(i)+"/hora");
+  }
+}
 
 /***************************************************************************************/
 void MOSTRA_LocalTime() //Função para imprimir o tempo local
@@ -63,11 +84,11 @@ int get_LocalTimeMinuto()
   return timeinfo.tm_min;
 }
 /***************************************************************************************/
-bool CHECA_HORARIO(int rotina[tamanho_rotina][2])
+bool ChecaHorario(int rotina[tam_list])
 {
   for(int i = 0; i < 10; i++)
    {
-      if(get_LocalTimeHora() == rotina[i][0] && get_LocalTimeMinuto() == rotina[i][1])
+      if(get_LocalTimeHora() == rotina[i].hora && get_LocalTimeMinuto() == rotina[i].minuto)
       {
         Serial.println("DEU O HORÁRIO");
         return true;
@@ -76,7 +97,7 @@ bool CHECA_HORARIO(int rotina[tamanho_rotina][2])
    return false;
 }
 /***************************************************************************************/
-void CONECTA_WIFI() //Função para conectar no wifi
+void ConectaWiFi() //Função para conectar no wifi
 {
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
@@ -87,7 +108,7 @@ void CONECTA_WIFI() //Função para conectar no wifi
   Serial.println(" CONNECTED");
 }
 /***************************************************************************************/
-void MOSTRA_HORARIO()
+void MostraHorario()
 {
   Serial.print((String)get_LocalTimeHora() + " : " + (String)get_LocalTimeMinuto() + '\n');
 }
@@ -98,7 +119,8 @@ void setup() //Função de configuração
 {
   Serial.begin(115200);
   
-  CONECTA_WIFI();
+  ConectaWiFi();
+  Firebase.begin(firebase_host, firebase_auth);
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //Conecta no servidor e obtem o horario local
   MOSTRA_LocalTime();
 
@@ -106,16 +128,13 @@ void setup() //Função de configuração
 
 void loop() //Função principal de execução
 {
-  MOSTRA_HORARIO();
+  AtualizaLista();
+  MostraHorario();
   Serial.println('.');
   delay(1000);
 
   //Checa firebase
   
-  //Verifica rotina
-  if (CHECA_HORARIO(horario_rotina))
-  {
-    //aciona os motores dos compartimentos
-  }
+}
   
 }
